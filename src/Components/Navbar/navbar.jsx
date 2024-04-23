271cec8281af2b2ab6775004c25a57cb88927264
Vijay Digambar kusekar <vijay.kusekar2003@gmail.com>
2024-04-24 01:22:56 +0530

    0

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -14,7 +14,7 @@ import React, { useEffect, useState } from 'react'
 
 const Navbars = () => {
 
-  const [searchvalue, setsearchvalue] = useState(null);
+  const [searchvalue, setsearchvalue] = useState();
   const [searchdata, setsearchdata] = useState();
   const router = useRouter()
 
