5e1b607e031cc4c08737549cd2b099b0234a4cb7
Vijay Digambar Kusekar <vijay.kusekar2003@gmail.com>
2024-04-23 22:34:00 +0530

    Update HeaderBanner.jsx

--- src/Components/HeaderBanner.jsx
+++ src/Components/HeaderBanner.jsx
@@ -2,7 +2,6 @@
 import { useEffect, useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import { useRouter } from 'next/navigation';
-// import ExampleCarouselImage from 'components/ExampleCarouselImage';
 
 function ControlledCarousel() {
 
